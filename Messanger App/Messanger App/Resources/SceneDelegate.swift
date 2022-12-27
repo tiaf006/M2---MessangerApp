6221a239301525e77d332c882a88e401c75de39a
taif <toofy.556@hotmail.com>
2022-12-27 04:43:24 -0800

    add firebase - TAIF

--- Messanger App/Messanger App/Resources/SceneDelegate.swift
+++ Messanger App/Messanger App/Resources/SceneDelegate.swift
@@ -16,12 +16,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let appScene = (scene as? UIWindowScene) else { return }
+       /* guard let appScene = (scene as? UIWindowScene) else { return }
         let launchVC = LoginViewController()
         window = UIWindow(windowScene: appScene)
         window?.rootViewController = launchVC
-        window?.makeKeyAndVisible()
-    }
+        window?.makeKeyAndVisible() */
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let LogInviewController = LoginViewController()
+            let navigationController = UINavigationController(rootViewController: LogInviewController)
+            window.rootViewController = navigationController
+                self.window = window
+                window.makeKeyAndVisible()
+            }    }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
